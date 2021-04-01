f06155c9ca46b65135fe33fe63e131436afb9fb3
Eduardo Lodi Marzano <lodi001@uol.com.br>
2021-04-01 15:57:21 -0300

    Changed buildspec again to try to build just one docker image

--- build.sh
+++ build.sh
@@ -2,6 +2,3 @@ rm -vf key_manager_grpc_docker.zip
 zip -r key_manager_grpc_docker.zip *
 aws s3 cp key_manager_grpc_docker.zip s3://pix-bucket-31032021/
 
-rm -vf key_manager_rest_docker.zip
-zip -r key_manager_rest_docker.zip *
-aws s3 cp key_manager_rest_docker.zip s3://pix-bucket-31032021/
